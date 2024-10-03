5902b99888278df3c7f36ad5a5a80e321cfc2d34
timeester3648 <weteringt@gmail.com>
2024-10-03 22:47:12 +0200

    Update

--- stb_image_resize.cpp
+++ stb_image_resize.cpp
@@ -1,8 +1,8 @@
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
 #include <MultiEngine/core/memory/MemoryAllocation.h>
 
-#define STBIR_MALLOC(sz)           MultiEngine::memory_allocate(sz)
-#define STBIR_REALLOC(p,newsz)     MultiEngine::memory_reallocate(p,newsz)
-#define STBIR_FREE(p)              MultiEngine::memory_free(p)
+#define STBIR_MALLOC(sz,u)           MultiEngine::memory_allocate(sz)
+#define STBIR_REALLOC(p,newsz,u)     MultiEngine::memory_reallocate(p,newsz)
+#define STBIR_FREE(p,u)              MultiEngine::memory_free(p)
 
 #include "stb_image_resize2.h"
